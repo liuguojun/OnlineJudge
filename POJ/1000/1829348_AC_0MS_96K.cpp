754e7a4771f617a623b3b9702d804d9a9f5b1e49
Guojun Liu <liuguojun.pku@gmail.com>
2012-04-05 23:28:09 +0800

    modified c++ format in  1000 and 1004

--- POJ/1000/1829348_AC_0MS_96K.cpp
+++ POJ/1000/1829348_AC_0MS_96K.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 using namespace std;
-int  main()
+
+int  main(int argc, char *argv[])
 {
-    int a,b;
+    int a, b;
     cin >> a >> b;
     cout << a+b << endl;
     return 0;
